36c98c6db5e4630573083515b3dd51271f7ab2eb
Phil Shaw <philshaw13@yahoo.com>
2012-09-27 11:21:53 -0600

    First commit. Everything working from built application using slides. Working on adding requirements from homeworkk.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'a00273816358d33adb62c160eb73f6218c88194ab2ef6c682e586a7c993f76335d4e3ccfb4ada1b128c10774cf166360613aab035b7a038a6f7b1e9be44aa36d'
